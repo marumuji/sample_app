cb48bbf052cd32d5fe73d6d0e5b74001f6befcd1
muji maru <marumuji@gmail.com>
2015-08-12 15:15:07 +0900

    Finish static pages

---
+++ spec/spec_helper.rb
@@ -0,0 +1,9 @@
+require 'capybara/rspec'
+
+RSpec.configure do |config|
+  config.include Capybara::DSL
+end
+
+ENV["RAILS_ENV"] ||= 'test'
+require File.expand_path("../../config/environment", __FILE__)
+require 'rspec/rails'
